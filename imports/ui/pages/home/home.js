296a7f9466995a119232d2de4abb2115c716ca2e
Dylan Dotolo <dotolo.dylan2@gmail.com>
2020-02-07 14:30:32 -0600

    connected a test api

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -2,3 +2,30 @@ import './home.html';
 
 import '../../components/hello/hello.js';
 import '../../components/info/info.js';
+
+// Template.App_home.helpers({
+//   Meteor.call('getPosts', function (err, res) {
+//     // The method call sets the Session variable to the callback value
+//     if (err) {
+//       Session.set('location', {error: err});
+//     } else {
+//       Session.set('location', res);
+//     return res;
+//     }
+//   });
+//  });
+
+Template.App_home.helpers({
+  location: function () {
+  //
+    return Session.get('location');
+  }
+ });
+
+Meteor.call('getPosts', function(err, res) {
+  if (err) {
+    Session.set('location', {error: err});
+  } else {
+    Session.set('location', res);
+  }
+})
